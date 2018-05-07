46b5c6f463b3e4f410f6386766003fefe1347025
Wok <woctezuma@users.noreply.github.com>
2018-05-07 10:07:29 +0200

    Initialize Python file for Bayesian average computation

---
+++ compute_bayesian_average.py
@@ -0,0 +1,12 @@
+from load_data import load_steamdb_data, load_steamspy_data
+
+def main():
+    steamdb_data = load_steamdb_data()
+
+    steamspy_data = load_steamspy_data()
+
+    return
+
+
+if __name__ == '__main__':
+    main()
