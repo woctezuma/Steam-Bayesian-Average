Fix prior computation for raw_score: - before, average the raw scores (which are average values themselves!), - now, compute the average value ONCE over the whole dataset.

compute_bayesian_average.py
@@ -51,7 +51,7 @@ def choose_prior(data, keyword=None):
         list_num_votes = [compute_dev_num_votes(dev) for dev in data.values()]
 
     prior = dict()
-    prior['raw_score'] = np.average(list_raw_scores)
+    prior['raw_score'] = np.sum(list_increment_values) / np.sum(list_num_votes)
     prior['num_votes'] = np.median(list_num_votes)
 
     return prior